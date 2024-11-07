87b7779e76aaffde8d36c9a7f4e0cade84492605
ahmsoy <136300225+ahmsoy@users.noreply.github.com>
2024-11-06 20:20:51 -0600

    Add files via upload

---
+++ precache-manifest.68d27777880ae2af9f605d2df62c3f87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1174df6021c6614c2e3cd623b82632b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "204309ba69f91d541672",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "49b7ec7a8ef93a8d423a",
+    "url": "./static/css/main.797aa6d5.chunk.css"
+  },
+  {
+    "revision": "204309ba69f91d541672",
+    "url": "./static/js/2.29bb5116.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.29bb5116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b7ec7a8ef93a8d423a",
+    "url": "./static/js/main.3e5ae0ef.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
